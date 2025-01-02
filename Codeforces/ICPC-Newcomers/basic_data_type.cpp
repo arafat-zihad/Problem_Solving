683884a3910d0660c012c1bba7e49b9d08ffb52e
arafat-zihad <arafat.zihad01@gmail.com>
2025-01-02 21:11:17 +0600

    Add files via upload

---
+++ Codeforces/ICPC-Newcomers/basic_data_type.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    long long int lli;
+    char ch;
+    float f;
+    double d;
+    cin >> i >> lli >> ch >> f >> d;
+    cout << i << endl << lli<< endl<< ch << endl << f << endl << d <<endl;
+
+    return 0;
+}
