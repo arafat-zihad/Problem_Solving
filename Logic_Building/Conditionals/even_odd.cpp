2fe90e3bd78b6e7b53dbb7cc42c184605d06d0a8
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-05 20:05:40 +0600

    Create even_odd.cpp

---
+++ Logic_Building/Conditionals/even_odd.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x; 
+    cin >> x;
+    if(x % 2 == 0){
+        cout << "Even" << endl;
+    }
+    else cout << "Odd" << endl;
+    return 0;
+}
