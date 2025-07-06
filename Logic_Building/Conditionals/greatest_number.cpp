8473f6a4e7b99b1614511751ee38b214a1a6b97d
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-06 16:35:51 +0600

    greatest number solved

---
+++ Logic_Building/Conditionals/greatest_number.cpp
@@ -0,0 +1,20 @@
+/*Take 3 positibe interger input and print the greatest of them*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a > b && a > c){
+        cout << a << " a is the greatest" << endl;
+    }
+    else if(b > a && b > c){
+        cout << b << " b is the greatest" << endl;
+    }
+    else {
+        cout << c << " is the greatest" << endl;
+    }
+    
+    return 0;
+}
