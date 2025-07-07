79b03f707dae54c5bf3343110e4a42a03716c4c6
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-07 14:38:56 +0600

    Loop practice started

---
+++ Logic_Building/Loop/print_even.cpp
@@ -0,0 +1,13 @@
+/*Print all the even numbers form from 1 to n*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
