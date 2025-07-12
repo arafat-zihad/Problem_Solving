5594045012b168a9fd077cdf3fe89476b250362a
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-12 09:38:10 +0600

    continue statement solve

---
+++ Logic_Building/Loop/continue_statement.cpp
@@ -0,0 +1,15 @@
+//write a program to print odd no from 1 to 100
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i <= n; i++){
+        if(i % 2 == 0){
+            continue;
+        }
+        cout << i <<" ";
+    }
+    return 0;
+}
