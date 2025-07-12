331eb7334b8621d262017a136ebb88ac413fa7a6
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-12 10:50:20 +0600

    sum of digit solved

---
+++ Logic_Building/Loop/sum_of_digit.cpp
@@ -0,0 +1,15 @@
+//WAP to print sum of digits of a given number
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, sum = 0;
+    cin >> n;
+
+    while(n != 0){
+        sum = sum + (n % 10);
+        n = n / 10;
+    }
+    cout << sum;
+    return 0;
+}
