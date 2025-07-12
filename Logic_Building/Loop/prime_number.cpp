fc8260e08c48532af9ae9a2dd42f5a5f0e1d8d59
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-12 09:27:13 +0600

    Prime number using break statement

--- Logic_Building/Loop/prime_number.cpp
+++ Logic_Building/Loop/prime_number.cpp
@@ -4,15 +4,30 @@ using namespace std;
 int main(){
     int n, cnt = 0;
     cin >> n;
-    for(int i = 2; i < n; i++){
+    // for(int i = 2; i < n; i++){
+    //     if(n % i == 0){
+    //         cnt++;
+    //     }
+    // }
+    // if(cnt >= 1){
+    //     cout << "Prime";
+    // } else{
+    //     cout << "Not Prime";
+    // }
+
+    //Solve using Breck statement
+    for(int i = 2; i <= n -1; i++){
         if(n % i == 0){
-            cnt++;
+            cnt = 1;
+            break;
         }
     }
-    if(cnt >= 1){
+    if( cnt == 0){
         cout << "Prime";
-    } else{
-        cout << "Not Prime";
     }
+    else{
+        cout <<"Not prime";
+    }
+
     return 0;
 } 
