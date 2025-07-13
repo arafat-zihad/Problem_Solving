72fac8a574f4584fc8e7f8b510bbedb573d68201
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-13 10:42:19 +0600

    Add implementation for swapping two numbers without a third variable

---
+++ Logic_Building/Function/3_swap_without_variable.cpp
@@ -0,0 +1,16 @@
+//Swap 2 numbers without third variable
+
+#include <bits/stdc++.h>
+using namespace std;
+void sw(int &a, int &b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    return;
+}
+int main(){
+    int x = 3, y = 5;
+    sw(x, y);
+    cout << "x = " << x << ", y = " << y;
+    return 0;
+}
