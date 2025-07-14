e46c14b37fca1ddf5d486f4eef80643ab3f0c5a9
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-14 19:26:20 +0600

    Add implementation for addition function and related output files

---
+++ Logic_Building/Function/5_add.cpp
@@ -0,0 +1,17 @@
+//ICPC AUC Group - Fuction problem 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b){
+    int result = a + b;
+
+    return result;
+}
+
+int main(){
+    int x, y; cin >> x >> y;
+    int result = add(x, y);
+    cout << result;
+    return 0;
+}
