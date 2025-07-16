98102def57def6dd10e86e31bf02bbddfe2ff42f
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-16 21:41:45 +0600

    solved

--- Logic_Building/Recursion/3_print_reverse.cpp
+++ Logic_Building/Recursion/3_print_reverse.cpp
@@ -3,13 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int n, int i){
-    if(n < 1) return;
-    cout << n << endl;
-    print(n-1, i);
+void print(int i){
+    if(i < 1) return;
+    cout << i << endl;
+    print(i-1);
 }
 int main(){
-    int n; cin >> n;
-    print(n, 1);
+    int i; cin >> i;
+    print(i);
     return 0;
 }
