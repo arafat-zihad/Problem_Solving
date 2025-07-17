27a31af945808eed2d5dfd774ebd6c6ef91f6468
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-17 12:26:15 +0600

    backtraing print nunber solved

---
+++ Logic_Building/Recursion/4_print_no_backtraking.cpp
@@ -0,0 +1,15 @@
+//Print 1 to N using recursion and backtraking
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int i, int n){
+    if(i < 1) return;
+    print (i - 1, n);
+    cout << i << endl;
+}
+
+int main(){
+    int n; cin >> n;
+    print(n, n);
+    return 0;
+}
