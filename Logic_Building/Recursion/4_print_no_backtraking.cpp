3dad504650120c516c8b29e48f4920873b53e267
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-17 13:00:32 +0600

    backtaking n to 1

--- Logic_Building/Recursion/4_print_no_backtraking.cpp
+++ Logic_Building/Recursion/4_print_no_backtraking.cpp
@@ -1,15 +1,31 @@
 //Print 1 to N using recursion and backtraking
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void print(int i, int n){
+//     if(i < 1) return;
+//     print (i - 1, n);
+//     cout << i << endl;
+// }
+
+// int main(){
+//     int n; cin >> n;
+//     print(n, n);
+//     return 0;
+// }
+
+//Print N to 1 using recursion and backtraking
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int i, int n){
-    if(i < 1) return;
-    print (i - 1, n);
+void print(int i, int x){
+    if(i > x) return;
+    print (i + 1, x);
     cout << i << endl;
 }
 
 int main(){
     int n; cin >> n;
-    print(n, n);
+    print(1, n);
     return 0;
 }
