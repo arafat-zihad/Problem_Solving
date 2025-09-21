d1f7129bcae48967295d18a93cdef32e308f2333
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-21 20:53:49 +0600

    array problem 2 solved

--- Logic_Building/Array/problem02.cpp
+++ Logic_Building/Array/problem02.cpp
@@ -1,8 +1,13 @@
-//Calculate the sum of all the elemet
+//Calculate the sum of all the elemets given array.
 
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int arr[5]= {1, 3, 4, 9, 100};
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        sum += arr[i];
+    }
+    cout << sum  << endl;
     return 0;
 }
