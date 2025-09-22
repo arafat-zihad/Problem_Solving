5333fc48353559547d9a0f3ac85836891a479733
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-22 10:41:23 +0600

    array problem 6 solved

---
+++ Logic_Building/Array/problem06.cpp
@@ -0,0 +1,25 @@
+/*Count the number of elements in the given array g
+than a given number x.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    vector <int> arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int x; cin >> x;
+    int cnt =0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > x){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
