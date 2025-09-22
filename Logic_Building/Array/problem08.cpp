2e7d1b8cbb8410993f132cecafaadcecae183a2a
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-22 11:30:46 +0600

    problem 08 solved

---
+++ Logic_Building/Array/problem08.cpp
@@ -0,0 +1,37 @@
+/*Find the total number of pairs in the Array whose
+sum is equal to the given value x */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    vector<int>arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int x; cin >> x;
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] + arr[j] == 12){
+                cout << "(" << arr[i] << "," << arr[j] << "), ";
+                cnt++;
+            }
+    }
+    }
+    cout << cnt << endl;
+
+    //Little change of different output
+
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < n; j++){
+    //         if(arr[i] + arr[j] == 12){
+    //             cout << "(" << arr[i] << "," << arr[j] << "), ";
+    //             cnt++;
+    //         }
+    // }
+    // }
+    // cout << cnt << endl;
+
+    return 0;
+}
