52b48afd88200465d6d13769991a854a69b1d367
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-22 15:54:47 +0600

    problem 10 solved

--- Logic_Building/Array/problem10.cpp
+++ Logic_Building/Array/problem10.cpp
@@ -12,17 +12,25 @@ int main(){
     int max = INT_MIN;
     int smax = INT_MIN;
 
-    for(int i = 0; i < n; i++){
+//Uiing Single loop
+     for(int i = 0; i < n; i++){
         if(arr[i] > max)
+        smax = max;
         max = arr[i];
     }
 
-    for(int i = 0; i < n; i++){
-        if(arr[i] != max && arr[i] > smax){
-            smax = arr[i];
-        }
-    }
-    cout << smax;
+// Using two loop
+    // for(int i = 0; i < n; i++){
+    //     if(arr[i] > max)
+    //     max = arr[i];
+    // }
+
+    // for(int i = 0; i < n; i++){
+    //     if(arr[i] != max && arr[i] > smax){
+    //         smax = arr[i];
+    //     }
+    // }
+    cout <<"Second Max is: " <<  smax << endl;
 
     return 0;
 }
