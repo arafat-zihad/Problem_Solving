ebc70b1d9b1db03f74d4268880fade39927f1cf8
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-27 10:21:06 +0600

    add two matrix solved

---
+++ Logic_Building/2D_Array/p02_addTwo.cpp
@@ -0,0 +1,40 @@
+//Write a program to add two matrices
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int r, c;
+    cout << "Enter Raw and Column: ";
+    cin >> r >> c;
+    vector<vector<int>>arr(r, vector<int>(c));
+    vector<vector<int>>brr(r, vector<int>(c));
+    vector<vector<int>>result(r, vector<int>(c));
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> brr[i][j];
+        }
+    }
+
+
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            result[i][j] = arr[i][j] + brr[i][j];
+        }
+    }
+
+    cout << endl;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
