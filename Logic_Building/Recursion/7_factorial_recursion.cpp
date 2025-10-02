6ef231c000785f8f93c81cd9d942ba263c91b531
arafat-zihad <arafat.zihad01@gmail.com>
2025-10-02 12:38:01 +0600

    coded

--- Logic_Building/Recursion/7_factorial_recursion.cpp
+++ Logic_Building/Recursion/7_factorial_recursion.cpp
@@ -3,12 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int n){
+int fact(int n){
     if(n== 0) return 1;
-    return n * sum (n - 1);
+    return n * fact(n - 1);
 }
+
 int main(){
     int n; cin >> n;
-    cout << sum(n);
+    cout << fact(n);
     return 0;
 }
