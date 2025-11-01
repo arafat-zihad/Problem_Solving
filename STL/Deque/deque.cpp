7537d52c96c5cb0d77e2defd70193d4007b99715
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-01 23:32:27 +0600

    Queue

---
+++ STL/Deque/deque.cpp
@@ -0,0 +1,26 @@
+// deque is a container like Vector, List
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    /*
+    1. Deque Declaration
+    2. push_back()
+    3. push_fron()
+    4. how to print deque value
+    5. front()
+    6. back()
+    7. pop_front()
+    8. pop_back()
+    9. size()
+    10. empty()
+    11. clear()
+    12. erase()
+    13. inser()
+    */
+
+    deque <int> dq;
+    dq.push_back(1);
+    cout << dq.size() << endl;
+    return 0;
+}
+
